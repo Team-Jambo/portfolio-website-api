5c68b5583c651635f93cda9179d1db71d5e054ce
Offet <nuellaemmanuella7003@gmail.com>
2024-07-09 11:27:52 +0000

    new codes added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// index file
+import express from "express";
+
+import { dbConnection } from "./config/db.js";
+
+// initialise express
+const app = express();
+
+// instantiate the connection
+dbConnection();
+
+// define a port 
+const port = process.env.PORT || 8000;
+
+// Listen to them on the port
+app.listen(port, () => {
+    console.log(`App is listening at port ${port}!`);
+});
